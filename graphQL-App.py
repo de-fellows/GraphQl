5d52e35755aabc4a7ab087002d194c22be5d3952
zeiadkhamees <zeiadahmedsameeh.kha@ucalgary.ca>
2022-07-01 21:18:17 -0600

    adjusted mutation class

--- graphQL-App.py
+++ graphQL-App.py
@@ -1,18 +1,18 @@
 from flask import Flask
+from flask import session
 from flask_sqlalchemy import SQLAlchemy
 import graphene 
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from flask_graphql import GraphQLView
-import json
 
 
 
 app = Flask(__name__)
 
 
+# configuring the SQL database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
 db = SQLAlchemy(app)
-
 
 
 class Music(db.Model):
@@ -28,35 +28,44 @@ class Music(db.Model):
 class MusicObject(SQLAlchemyObjectType):
     class Meta:
        model = Music
-       interfaces = (graphene.relay.Node) 
+       interfaces = (graphene.relay.Node, )
 
 
-
+# Query class 
 class Query(graphene.ObjectType):
     node = graphene.relay.Node.Field()
-    all_music = SQLAlchemyConnectionField(MusicObject)
+    all_music = SQLAlchemyConnectionField(MusicObject.connection)
 
 
 
+# Creates entries into the database
 class CreateMusic(graphene.Mutation):
-    music = graphene.Field(MusicObject)
+
     class Arguments:
-        name =graphene.String(required=True)
-        description =graphene.String(required=True)
-    
-    def mutate(self, info, name, description):
-        music = Music(name=name, description=description)
+        name = graphene.String(required=True)
+        description = graphene.String(required=True)
+
+    music = graphene.Field(lambda: MusicObject)
+
+    def mutate(self, info, name, description):    # method to be called when writing data
+        music = Music(
+            name = name,
+            description = description
+        )
         if music:
             db.session.add(music)
             db.session.commit()
+
         return CreateMusic(music=music)
 
 
 
+# Mutation class 
 class Mutation(graphene.ObjectType):
     create_music = CreateMusic.Field()
 
 
+# defining the schema 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 
 
@@ -65,23 +74,7 @@ def index():
     return "<h1>Music Genres!</h>"
 
 
-@app.route("/music")
-def get_music():
-    music = Music.query.all()
 
-    output = []
-    for genre in music :
-        music_info = {'name': genre.name, 'description': genre.description}
-
-        output.append(music_info)
-
-    return {"music": output}
-
-
-@app.route('/music/<id>')
-def get_music_id(id):         
-    music = Music.query.get_or_404(id)         # this will either fetch the drink with a specific id or return a 404 error
-    return {"name": music.name, "description":music.description}
 
 app.add_url_rule(
     '/graphql',
@@ -92,5 +85,6 @@ app.add_url_rule(
     )
 )
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
